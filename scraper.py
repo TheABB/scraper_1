1e8b87681f7e009bfa50e11ca0ceb60009633923
TheABB <jenpersson9@gmail.com>
2016-01-20 16:23:04 +0000

    sandbox test

--- scraper.py
+++ scraper.py
@@ -1,6 +1,8 @@
 # This is a template for a Python scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 print 'hello, world.'
+urltoscrape='https://twitter.com/TheABB'
+print urltoscrape
 # import scraperwiki
 # import lxml.html
 #
