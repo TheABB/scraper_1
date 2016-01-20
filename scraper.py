742d38a3dbfa86c55f0986ac7d5cd40e8ae8feb5
TheABB <jenpersson9@gmail.com>
2016-01-20 16:18:20 +0000

    My first simple command line

--- scraper.py
+++ scraper.py
@@ -1,6 +1,6 @@
 # This is a template for a Python scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
-
+print 'hello, world.'
 # import scraperwiki
 # import lxml.html
 #
